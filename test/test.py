480b22c948e8d54e9a5c7a686c7405850cb809e0
Your Name <you@example.com>
2024-07-01 13:43:11 +0200

    n

--- test/test.py
+++ test/test.py
@@ -1,2 +1,4 @@
-import streamlit
-print(streamlit.__version__)
+import pickle
+resources_web_app = pickle.load(open('resources/resources_web_app.pickle', 'rb'))
+print(resources_web_app['columns_to_plot_people_dependent'])
+print(resources_web_app['columns_to_plot_non_people_dependent_barplot'] )
